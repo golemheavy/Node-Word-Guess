2bc830766bd3af52a9679f8298c2b88689be1beb
Steve Long <steve@gridtop.com>
2019-05-08 14:05:41 -0500

    Letter.js initial design

---
+++ Letter.js
@@ -0,0 +1,15 @@
+function Letter(letterVal ) {
+	this.letterVal = letterVal.toString().trim().slice(0,1).toLowerCase();
+	this.guessed = false;
+	this.returnLetter = function() {
+		if (this.guessed === true) return this.letterVal;
+		else return "_";
+	}
+	this.checkGuess = function(l) {
+		console.log(l.charAt(0));
+		console.log(this.letterVal);
+		if (this.guessed === false && l.trim().charAt(0).toLowerCase() === this.letterVal) this.guessed = true;
+	}
+}
+
+let xlet = new Letter("c");
