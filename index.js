6f873a7ee6b5c07001f831ba6e059003814c6c39
Steve Long <steve@gridtop.com>
2019-05-08 17:38:25 -0500

    still an issue with accepting keyboard input for guessed letters, but this item is improving

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var word = require("./Word.js");
 
-function GameObject(guesses) {
-	this.wordlist = [
+var wordListObj = {
+	wordlist : [
 		"signatory",
 		"ecumenical",
 		"dormitory",
@@ -10,43 +10,25 @@ function GameObject(guesses) {
 		"antidisestablishmentarianism",
 		"merovingian",
 		"alpine"
-	];
-	this.getRandomWord = function() {
+	],
+	getRandomWord : function() {
 		return this.wordlist[Math.floor(Math.random() * this.wordlist.length)];
-	};
+	}
+}
+	
+function GameObject(guesses) {
+
 	this.userGuessesRemaining = guesses;
 	this.lettersChecked = "";
+	this.currentWord = new word(wordListObj.getRandomWord());
 }
 
-/*
-gamobj = new GameObject(5);
-console.log(gamobj);
-gamobj.currentWord = new word(gamobj.getRandomWord());
-*/
-
-/*
-let x = 0;
-for (x in gamobj.currentWord.letterArray) {
-	console.log(gamobj.currentWord.letterArray[x] + ""); // this causes type coercian, which calls the overloaded toString function I defined for my Letter constructor. This causes the placeholder to read out instead of the defined letter, unless the letter has been guessed.
-};
-*/
-
-/*
-while (true) {
-	
-	let guesses = parseInt(prompt("How many guesses?"));
-	if (guesses > 0 && guesses < 12) {let gamobj = new GameObject(guesses);}
-	else console.log("please enter a value of guesses between 1 and 12");
-}
-*/
 console.log("Please enter a number of guesses:");
 
 const readline = require('readline');
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
-
-
 
 let promise1 = new Promise(function(resolve, reject) {
 	let entry = "";
@@ -64,9 +46,7 @@ let promise1 = new Promise(function(resolve, reject) {
 		console.log(val + " guesses entered.");
 		let guesses = Math.floor(parseFloat(val));
 		if (guesses > 0 && guesses < 12) {
-			let gamobj = new GameObject(guesses);
-			gamobj.currentWord = new word(gamobj.getRandomWord());
-			main(gamobj);
+			main(new GameObject(guesses));
 		}
 		else console.log("please enter a value of guesses between 1 and 12");
 		process.exit()
