cd69c581795706238aaad15e31aaea9803f248ef
Steve Long <steve@gridtop.com>
2019-05-08 16:26:55 -0500

    getting key entry working

--- index.js
+++ index.js
@@ -17,14 +17,56 @@ function GameObject(guesses) {
 	this.userGuessesRemaining = guesses;
 }
 
+/*
 gamobj = new GameObject(5);
 console.log(gamobj);
 gamobj.currentWord = new word(gamobj.getRandomWord());
+*/
 
+/*
 let x = 0;
 for (x in gamobj.currentWord.letterArray) {
 	console.log(gamobj.currentWord.letterArray[x] + ""); // this causes type coercian, which calls the overloaded toString function I defined for my Letter constructor. This causes the placeholder to read out instead of the defined letter, unless the letter has been guessed.
 };
+*/
 
+/*
+while (true) {
+	
+	let guesses = parseInt(prompt("How many guesses?"));
+	if (guesses > 0 && guesses < 12) {let gamobj = new GameObject(guesses);}
+	else console.log("please enter a value of guesses between 1 and 12");
+}
+*/
+console.log("Please enter a number of guesses:");
+
+const readline = require('readline');
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+
+
+let promise1 = new Promise(function(resolve, reject) {
+	let entry = "";
+	process.stdin.on('keypress', (str, key) => {
+		if ("0123456789".includes(str)) {
+			process.stdout.write(str);
+			entry += str;
+		}
+		if ( key.sequence === '\u0003' || key.sequence === '\r' ) {  // stop reading stdin if control-c or return is pressed
+			resolve(entry);
+		}		
+	});
+}).then(function(val){
+		console.log("\n");
+		console.log(val + " guesses entered.");
+		let guesses = Math.floor(parseFloat(val));
+		if (guesses > 0 && guesses < 12) {let gamobj = new GameObject(guesses); console.log(gamobj);}
+		else console.log("please enter a value of guesses between 1 and 12");
+		process.exit()
+});
+
+//console.log(promise1);
 
 
